6219977f19126cf53f9b33b7de2c3e5a4915817d
Lance Bontrager <87950314+1ance-301@users.noreply.github.com>
2021-10-29 17:49:37 -0400

    comments functions

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 
 router.get('/', (req, res) => {
-    Comment.findAll({
-
-    })
+    Comment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.post('/', (req, res) => {
@@ -22,8 +25,21 @@ router.post('/', (req, res) => {
 
 router.delete('/:id', (req, res) => {
     Comment.destroy({
-        
+        where: {
+            id: req.params.id
+        }
     })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'No comment found with this id!' });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
