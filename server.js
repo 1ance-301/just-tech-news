14cf8daf88b35d992d22fb965f12f36bef0cc5a7
Lance Bontrager <87950314+1ance-301@users.noreply.github.com>
2021-11-04 17:58:38 -0400

    dashboard

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const session = require('express-session');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
